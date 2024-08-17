1f3dd3edf5da6de46ea74a8f164f711e11070684
abhay <abhishekdubey660@gmail.com>
2024-08-17 23:53:58 +0530

    firebase Auth working on signup page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,4 +48,6 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     implementation("de.hdodenhof:circleimageview:3.1.0")
+    implementation(platform("com.google.firebase:firebase-bom:33.1.2"))
+    implementation("com.google.android.gms:play-services-auth:21.2.0")
 }
--- app/src/main/java/com/firefuro/todoapp/MainActivity.kt
+++ app/src/main/java/com/firefuro/todoapp/MainActivity.kt
@@ -1,19 +1,25 @@
 package com.firefuro.todoapp
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.provider.ContactsContract.CommonDataKinds.Email
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var databaseReference: DatabaseReference
+    lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,12 +29,13 @@ class MainActivity : AppCompatActivity() {
         val Password=findViewById<EditText>(R.id.Password)
         val SignUpBtn=findViewById<Button>(R.id.SignUpBtn)
         val hereText=findViewById<TextView>(R.id.clickHere)
+        auth= FirebaseAuth.getInstance()
 
 
 
 
 
-        databaseReference=FirebaseDatabase.getInstance().getReference("Users")
+//        databaseReference=FirebaseDatabase.getInstance().getReference("Users")
 
         SignUpBtn.setOnClickListener {
 
@@ -42,10 +49,25 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this,"Please Fill Every Detail Column",Toast.LENGTH_SHORT).show()
 
             }else{
-                databaseReference.child(Email).setValue(users)
-                Toast.makeText(this,"Database Added",Toast.LENGTH_SHORT).show()
-                val intent = Intent(this,Sign_In_Screen::class.java)
-                startActivity(intent)
+//                databaseReference.child(Email).setValue(users)
+                auth.createUserWithEmailAndPassword(Email, password)
+                    .addOnCompleteListener(this){ task ->
+                        if (task.isSuccessful) {
+                            // Sign in success, update UI with the signed-in user's information
+                            val intent = Intent(this,Sign_In_Screen::class.java)
+                            startActivity(intent)
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(
+                                baseContext,
+                                "Authentication failed.",
+                                Toast.LENGTH_SHORT,
+                            ).show()
+                        }
+                    }
+//                Toast.makeText(this,"Database Added",Toast.LENGTH_SHORT).show()
+
             }
 
 
@@ -54,6 +76,8 @@ class MainActivity : AppCompatActivity() {
             var intent = Intent(this,Sign_In_Screen::class.java)
             startActivity(intent)
         }
+
+
 
 
     }
