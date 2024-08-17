9ac1f1fce917fb06282104d7c4c4cac88cfdeaa7
abhay <abhishekdubey660@gmail.com>
2024-08-18 01:25:12 +0530

    App with integrated Firebase Auth

--- app/src/main/java/com/firefuro/todoapp/Sign_In_Screen.kt
+++ app/src/main/java/com/firefuro/todoapp/Sign_In_Screen.kt
@@ -6,17 +6,20 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 
 class Sign_In_Screen : AppCompatActivity() {
     lateinit var databaseReference: DatabaseReference
+    lateinit var sauth:FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in_screen)
         var enteredEmail = findViewById<EditText>(R.id.signinEmail)
         var enteredPassword = findViewById<EditText>(R.id.signinPassword)
         var SignInButton = findViewById<Button>(R.id.SignInButton)
+        sauth= FirebaseAuth.getInstance()
 
         SignInButton.setOnClickListener {
             var EmailVariable = enteredEmail.text.toString()
@@ -32,24 +35,34 @@ class Sign_In_Screen : AppCompatActivity() {
     }
 
     fun readDatabase(email:String , password:String){
-        databaseReference= FirebaseDatabase.getInstance().getReference("Users")
-        databaseReference.child(email).get().addOnSuccessListener {
-            if (it.exists()){
-                databaseReference.child(email).child("passwordData").get().addOnSuccessListener {
-                    if (it.exists()){
-                       val intent=Intent(this,HomePage::class.java)
-                        startActivity(intent)
-                        Toast.makeText(this,"Successfully Logged In",Toast.LENGTH_SHORT).show()
-
-                    }else{
-                        Toast.makeText(this,"Password or Email Id is Wrong",Toast.LENGTH_LONG).show()
-                    }
+        sauth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this) {task->
+            if (task.isSuccessful){
+                val intent=Intent(this,HomePage::class.java)
+                startActivity(intent)
+                Toast.makeText(this,"Successfully Logged In",Toast.LENGTH_SHORT).show()
             }
-
-            }else{Toast.makeText(this,"Email ID does not Exist in Database",Toast.LENGTH_LONG).show()}
-
-        }.addOnFailureListener{
-
+            else{
+                Toast.makeText(this,"Password or Email Id is Wrong",Toast.LENGTH_LONG).show()
+            }
         }
+//        databaseReference= FirebaseDatabase.getInstance().getReference("Users")
+//        databaseReference.child(email).get().addOnSuccessListener {
+//            if (it.exists()){
+//                databaseReference.child(email).child("passwordData").get().addOnSuccessListener {
+//                    if (it.exists()){
+//                       val intent=Intent(this,HomePage::class.java)
+//                        startActivity(intent)
+//                        Toast.makeText(this,"Successfully Logged In",Toast.LENGTH_SHORT).show()
+//
+//                    }else{
+//                        Toast.makeText(this,"Password or Email Id is Wrong",Toast.LENGTH_LONG).show()
+//                    }
+//            }
+//
+//            }else{Toast.makeText(this,"Email ID does not Exist in Database",Toast.LENGTH_LONG).show()}
+//
+//        }.addOnFailureListener{
+//
+//        }
     }
 }
